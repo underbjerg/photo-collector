2ea4631c67aa5bac2b62c4bd569b736bde57ec10
Erik Underbjerg <erik@underbjerg.com>
2014-08-21 09:29:15 +0200

    Made S3 images public, so they can be cached

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,7 +16,7 @@ CarrierWave.configure do |config|
     }
     
     config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
-    config.fog_public     = false                                   # optional, defaults to true
+    config.fog_public     = true                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     
     config.storage = :fog
