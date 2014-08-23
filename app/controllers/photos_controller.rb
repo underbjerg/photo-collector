5a40354f170e9e0752e8ff9a812b84f0c8b0cd3c
Erik Underbjerg <erik@underbjerg.com>
2014-08-24 01:19:11 +0200

    Minor bugfixes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -62,7 +62,7 @@ class PhotosController < ApplicationController
   def update
     respond_to do |format|
       if @photo.update(photo_params)
-        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
+        format.html { redirect_to [@photo.album, @photo], notice: 'Photo was successfully updated.' }
         format.json { render :show, status: :ok, location: @photo }
       else
         format.html { render :edit }
@@ -76,7 +76,7 @@ class PhotosController < ApplicationController
   def destroy
     @photo.destroy
     respond_to do |format|
-      format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
+      format.html { redirect_to album_photos_url(@photo.album), notice: 'Photo was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
