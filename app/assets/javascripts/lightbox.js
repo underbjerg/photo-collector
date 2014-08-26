5e76e6321bed6a9652b7c4cdaa7d72bff137dfa6
Erik Underbjerg <erik@underbjerg.com>
2014-08-26 23:45:55 +0200

    Made images appear bigger in lightbox on iPhone

--- app/assets/javascripts/lightbox.js
+++ app/assets/javascripts/lightbox.js
@@ -3,11 +3,17 @@ function initializeLightbox() {
 	$('#photo-container').magnificPopup({
 	  delegate: 'a.view', // child items selector, by clicking on it popup will open
 	  type: 'image',
+	  closeOnContentClick: true,
 	  // other options
 	  gallery: {enabled:true},
+	  mainClass: 'mfp-img-mobile mfp-no-margins',
+	  closeBtnInside: false,
+	  fixedContentPos: true,
+	  //mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
 	  image: {
+		  verticalFit: true,
 		  titleSrc: function(item) {
-		  	return  " <a href='" + item.el.attr('data-fullsize') + "' download class='' title='Download image'>DOWNLOAD</a>"
+		  	return  " <a href='" + item.el.attr('data-fullsize') + "' download class='' title='Download image'>DOWNLOAD</a> &middot; "
 					+ " " + item.el.attr('title')
 					+'<small>' 
 						+ item.el.attr('data-capture-time') 					
